3f20cd5008da3db445fb252a5a928780f7d2ff27
Benjamin Russell <barussell77@gmail.com>
2019-02-03 12:01:11 -0600

    building base files

---
+++ src/components/content.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content-grid">
+    <myinput/>
+    <myoutput/>
+  </div>
+</template>
+
+<script>
+import input from "./input.vue";
+import output from "./output.vue";
+export default {
+  components: {
+    input: "myinput",
+    output: "myoutput"
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
